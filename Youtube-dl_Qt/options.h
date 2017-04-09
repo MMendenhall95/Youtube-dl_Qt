d3a8913f0971e29a00975a54aa06813e030a879b
mike <mmendenhall95@gmail.com>
2017-04-09 11:56:41 -0700

    Download directory framework

--- Youtube-dl_Qt/options.h
+++ Youtube-dl_Qt/options.h
@@ -1,14 +1,16 @@
 #ifndef OPTIONS_H
 #define OPTIONS_H
-
+#include <QString>
 
 class Options
 {
 public:
-    Options(bool clean);
+    Options(bool clean, QString downloadDirectory);
     bool isClean();
+    QString getDirectory();
 private:
     bool clean;
+    QString downloadDirectory;
 };
 
 #endif // OPTIONS_H
