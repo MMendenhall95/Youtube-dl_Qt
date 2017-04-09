8493247aa730b1ebf6b377a4011cb889f863f8bc
mike <mmendenhall95@gmail.com>
2017-04-09 11:57:12 -0700

    download directory option in options

--- Youtube-dl_Qt/options.cpp
+++ Youtube-dl_Qt/options.cpp
@@ -1,10 +1,15 @@
 #include "options.h"
 
-Options::Options(bool clean)
+Options::Options(bool clean, QString downloadDirectory)
 {
     this->clean = clean;
+    this->downloadDirectory = downloadDirectory;
 }
 
 bool Options::isClean(){
     return clean;
+}
+
+QString Options::getDirectory(){
+    return downloadDirectory;
 }
